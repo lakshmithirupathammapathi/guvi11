ebce851384a22f43503249d948f6146b8234b42f
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-07-27 13:12:50 +0530

    Create guvi11.java

---
+++ guvi11.java
@@ -0,0 +1,13 @@
+int count = 0, num = 3452;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
+When you run the
